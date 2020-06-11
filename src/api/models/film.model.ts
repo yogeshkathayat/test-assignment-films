07c5324d316db000c8fb8b635470774468fac290
Yogesh <kathayat.yogesh@gmail.com>
2020-06-11 21:11:32 +0400

    fix: fix film schema

--- src/api/models/film.model.ts
+++ src/api/models/film.model.ts
@@ -5,18 +5,25 @@ import * as Joi from "@hapi/joi";
 export const FilmValidationSchema = Joi.object().keys({
     _id: Joi.string(),
     name: Joi.string().required(),
+    slugName: Joi.string(),
     description: Joi.string().required(),
     releaseDate: Joi.string().required(),
     rating: Joi.number().valid(1, 2, 3, 4, 5).required(),
     ticketPrice: Joi.string().required(),
     country: Joi.string().required(),
     genre: Joi.string().required(),
-    photo: Joi.string().required()
+    photo: Joi.string().required(),
+    comment:Joi.array()
 });
 
 
 const filmSchema = new Schema({
     name: {
+        type: String,
+        required: true,
+
+    },
+    slugName: {
         type: String,
         required: true,
 
@@ -42,17 +49,28 @@ const filmSchema = new Schema({
         required: true,
     },
     country: {
-        type: String,
+        type: Schema.Types.ObjectId,
+        ref: 'Country',
         required: true,
     },
-    genre: {
-        type: String,
-        required: true,
-    },
+    genre: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Genre',
+        required: true
+    }],
     photo: {
         type: String,
         required: true,
     },
+    comment: [{
+        text: {
+            type: String
+        },
+        user: {
+            type: Schema.Types.ObjectId,
+            ref: 'User',
+        }
+    }],
 }, { timestamps: true });
 
 export const FilmModel = model<IFilm>("Film", filmSchema);
